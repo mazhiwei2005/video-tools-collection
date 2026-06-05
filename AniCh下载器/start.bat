75298f7446e8512213d46011b19cd23cffaf6709
359923331 <359923331@qq.com>
2026-06-05 22:58:01 +0800

    Remove: B站上传工具, AniCh下载器, m3u8_downloader, 黑屏音频工具

--- AniCh下载器/start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-"C:\Users\Lenovo\anaconda3\python.exe" anich_downloader.py
-pause
--- B站上传工具/start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001 >nul 2>&1
-cd /d "%~dp0"
-echo ============================================
-echo   Bilibili Upload Tool (Playwright)
-echo ============================================
-echo.
-python bili_browser_uploader.py
